69053532a23b99df52a81aa61dd069e59c0c9821
yaaqin <yaqin.me@gmail.com>
2023-10-26 09:49:47 +0700

    [Edit] fixed format code

--- src/page/detail.js
+++ src/page/detail.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
 import "../style/App.css";
 import "../style/App.mobile.css";
 
-import Nav from '../component/nav'
-import BodyDetail from '../component/bodyDetail';
-import Footer from '../component/footer'
-
+import Nav from "../component/nav";
+import BodyDetail from "../component/bodyDetail";
+import Footer from "../component/footer";
 
 export default function detail() {
   return (
     <div>
-        <div className='container pt-4'>
-            <Nav />
+      <div className="container pt-4">
+        <Nav />
 
-       <BodyDetail />
-        </div>
-        
-      < Footer />
-        
+        <BodyDetail />
+
+        <Footer />
+      </div>
     </div>
-  )
+  );
 }
